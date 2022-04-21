3edd688549d0dbc46c8514e946ba58a15fe8a5c0
zhihonlin <zhihonl@clemson.edu>
2022-04-20 20:39:34 -0400

    Added Message System

--- Backend/inbox.php
+++ Backend/inbox.php
@@ -2,6 +2,12 @@
 // Include config file
 require_once "config.php";
 
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+$_SESSION['current_reply'] = 0;
+
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if (!isset($_SESSION["loggedin"])) {
     header("location: login.php");
@@ -11,89 +17,57 @@ if (!isset($_SESSION["loggedin"])) {
 // Define variables and initialize with empty values
 $receiver = $message = "";
 $receiver_err = $message_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Validate username
-    if(empty(trim($_POST["receiver"]))){
-        $receiver_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["receiver"]))){
-        $receiver_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT user_id FROM users_testing WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($con, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_receiver);
-            
-            // Set parameters
-            $param_receiver = trim($_POST["receiver"]);
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result */
-                mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 0){
-                    $receiver_err = "This user does not exist";
-                } else{
-                    $receiver = trim($_POST["receiver"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
 
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }
-    
+// Processing form data when form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate message
-    if(empty(trim($_POST["message"]))){
-        $message_err = "Please enter a message.";     
-    } elseif(strlen(trim($_POST["message"])) > 1000){
+
+    $_SESSION['current_reply'] = $_POST["message_id"];
+
+    if (empty(trim($_POST["message"]))) {
+        $message_err = "Please enter a message.";
+    } elseif (strlen(trim($_POST["message"])) > 1000) {
         $message_err = "Message cannot be greater than 1000 characters.";
-    } else{
+    } else {
         $message = trim($_POST["message"]);
     }
-    
+
     // Check input errors before inserting in database
-    if(empty($receiver_err) && empty($message_err)){
-        
+    if (empty($receiver_err) && empty($message_err)) {
+
         // Prepare an insert statement
         echo "hey we made it here";
-        $sql = "INSERT INTO message (sender, receiver,message) VALUES (?, ?, ?)";
-        
-        if($stmt = mysqli_prepare($con, $sql)){
+        $sql = "INSERT INTO reply (message_id, sender, receiver, message, date) VALUES (?, ?, ?, ?,?)";
+
+        if ($stmt = mysqli_prepare($con, $sql)) {
 
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sss", $param_sender, $param_receiver, $param_message);
+            mysqli_stmt_bind_param($stmt, "issss", $param_messageid, $param_sender, $param_receiver, $param_message, $param_date);
             // Set parameters
+            $param_messageid = $_POST["message_id"];
             $param_sender = $_SESSION["username"];
             $param_receiver = $receiver;
             $param_message = $message;
+            $param_date = $param_date = date('d-m-y H:i:s A');
 
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 // Redirect to login page
                 //$_SESSION['loggedin'] = true;
-                header("location: homepage.php");
-            } else{
+                header("location: inbox.php");
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
             // Close statement
             mysqli_stmt_close($stmt);
-        }
-        else{
+        } else {
             echo "this...";
         }
     }
-    
+
     // Close connection
-    mysqli_close($con);
+    //mysqli_close($con);
 }
 
 ?>
@@ -107,8 +81,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <!-- Material Icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <!-- CSS File -->
-    <link rel="stylesheet" href="styles/message.css" />
+    <link rel="stylesheet" href="styles/message.css?v=<?php echo time(); ?>" />
     <title>Youtube Clone with HTML & CSS</title>
+
+
+    <script>
+        function reply(ele) {
+            var current_message_id = ele.getAttribute('value');
+            console.log(current_message_id);
+
+            var reply_box = document.createElement('div');
+            reply_box.innerHTML = '<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>"> <div class="same-row"> <h3>Your Message:</h3> <textarea name="message" <?php echo (!empty($message_err)) ? 'is-invalid' : ''; ?>></textarea> <span class="invalid-feedback"><?php echo $message_err; ?></span> </div> <input type="submit" name="message_id" value=' + current_message_id + '> </form>';
+
+            submit_btn = document.createElement('div');
+
+            var reply_section = document.getElementsByClassName("message-section")[0];
+            reply_section.appendChild(reply_box);
+        }
+    </script>
 </head>
 
 <body>
@@ -225,26 +215,53 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="content-header">
 
                 <div class="tabs">
-                    <button >Send Message</button>
-                    <button style="color: black; border-bottom:2px solid black;">Inbox</button>
-                    <button id="outbox">Outbox</button>
+                    <button onclick="window.location.href='message.php'">Send Message</button>
+                    <button onclick="window.location.href='inbox.php'" style="color: black; border-bottom:2px solid black;" id="inbox">Inbox</button>
+                    <button onclick="window.location.href='outbox.php'" id="outbox">Outbox</button>
                 </div>
             </div>
 
             <div class="message-section">
-                <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-                    <div class="same-row">
-                        <h3>Recipient:</h3>
-                        <input type="text" name="receiver" <?php echo (!empty($receiver_err)) ? 'is-invalid' : ''; ?>>
-                        <span class="invalid-feedback"><?php echo $receiver_err; ?></span>
-                    </div>
-                    <div class="same-row">
-                        <h3>Your Message:</h3>
-                        <textarea name="message" <?php echo (!empty($message_err)) ? 'is-invalid' : ''; ?>></textarea>
-                        <span class="invalid-feedback"><?php echo $message_err; ?></span>
-                    </div>
-                    <input type="submit">
-                </form>
+                <?php
+                $sql = "SELECT * FROM message";
+                if ($result = mysqli_query($con, $sql)) {
+                    if (mysqli_num_rows($result) > 0) {
+
+                        while ($row = mysqli_fetch_array($result)) {
+                            if ($row['receiver'] == $_SESSION['username']) {
+                                echo "<div class='inbox-message'>";
+                                echo "<button id='reply' onclick='reply(this);'  value= {$row['message_id']} name='message_id'> Reply </button>";
+                                echo "<h3> Sender: " . $row['sender'] . "</h3>";
+                                echo "<h5> Date:" . $row['date'] . "</h5>";
+                                echo "<br>";
+                                echo "<p>" . $row['message'] . "</h5>";
+                                echo "</div>";
+                                $replies = "SELECT * FROM reply";
+                                if ($reply_result = mysqli_query($con, $replies)) {
+                                    if (mysqli_num_rows($reply_result) > 0) {
+                                        while ($reply_row = mysqli_fetch_array($reply_result)) {
+                                            if ($reply_row['message_id'] == $row['message_id']) {
+                                                echo "<div class='inbox-message reply'>";
+                                                echo "<button id='reply' onclick='reply(this);'  value= {$row['message_id']} name='message_id'> Reply </button>";
+                                                echo "<h3> Replier: " . $reply_row['sender'] . "</h3>";
+                                                echo "<h5> Date:" . $reply_row['date'] . "</h5>";
+                                                echo "<br>";
+                                                echo "<p>" . $reply_row['message'] . "</h5>";
+                                                echo "</div>";
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        mysqli_free_result($result);
+                    } else {
+                        echo "No message found";
+                    }
+                }
+                // Close connection
+                //mysqli_close($con);
+                ?>
             </div>
 
         </div>
--- Backend/message.php
+++ Backend/message.php
@@ -11,37 +11,36 @@ if (!isset($_SESSION["loggedin"])) {
 // Define variables and initialize with empty values
 $receiver = $message = "";
 $receiver_err = $message_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate username
-    if(empty(trim($_POST["receiver"]))){
+    if (empty(trim($_POST["receiver"]))) {
         $receiver_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["receiver"]))){
+    } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["receiver"]))) {
         $receiver_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
+    } else {
         // Prepare a select statement
         $sql = "SELECT user_id FROM users_testing WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($con, $sql)){
+
+        if ($stmt = mysqli_prepare($con, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_receiver);
-            
+
             // Set parameters
             $param_receiver = trim($_POST["receiver"]);
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 /* store result */
                 mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 0){
+
+                if (mysqli_stmt_num_rows($stmt) == 0) {
                     $receiver_err = "This user does not exist";
-                } else{
+                } else {
                     $receiver = trim($_POST["receiver"]);
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -49,49 +48,48 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    
+
     // Validate message
-    if(empty(trim($_POST["message"]))){
-        $message_err = "Please enter a message.";     
-    } elseif(strlen(trim($_POST["message"])) > 1000){
+    if (empty(trim($_POST["message"]))) {
+        $message_err = "Please enter a message.";
+    } elseif (strlen(trim($_POST["message"])) > 1000) {
         $message_err = "Message cannot be greater than 1000 characters.";
-    } else{
+    } else {
         $message = trim($_POST["message"]);
     }
-    
+
     // Check input errors before inserting in database
-    if(empty($receiver_err) && empty($message_err)){
-        
+    if (empty($receiver_err) && empty($message_err)) {
+
         // Prepare an insert statement
         echo "hey we made it here";
-        $sql = "INSERT INTO message (sender, receiver,message) VALUES (?, ?, ?)";
-        
-        if($stmt = mysqli_prepare($con, $sql)){
+        $sql = "INSERT INTO message (sender, receiver,message, date) VALUES (?, ?, ?, ?)";
+
+        if ($stmt = mysqli_prepare($con, $sql)) {
 
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "sss", $param_sender, $param_receiver, $param_message);
+            mysqli_stmt_bind_param($stmt, "ssss", $param_sender, $param_receiver, $param_message, $param_date);
             // Set parameters
             $param_sender = $_SESSION["username"];
             $param_receiver = $receiver;
             $param_message = $message;
-
+            $param_date = date('d-m-y H:i:s A');
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 // Redirect to login page
                 //$_SESSION['loggedin'] = true;
                 header("location: homepage.php");
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
             // Close statement
             mysqli_stmt_close($stmt);
-        }
-        else{
+        } else {
             echo "this...";
         }
     }
-    
+
     // Close connection
     mysqli_close($con);
 }
@@ -225,14 +223,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="content-header">
 
                 <div class="tabs">
-                    <button style="color: black; border-bottom:2px solid black;">Send Message</button>
-                    <button id="inbox">Inbox</button>
-                    <button id="outbox">Outbox</button>
+                    <button onclick="window.location.href='message.php'" style="color: black; border-bottom:2px solid black;">Send Message</button>
+                    <button onclick="window.location.href='inbox.php'" id="inbox">Inbox</button>
+                    <button onclick="window.location.href='outbox.php'" id="outbox">Outbox</button>
                 </div>
             </div>
 
             <div class="message-section">
-                <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                     <div class="same-row">
                         <h3>Recipient:</h3>
                         <input type="text" name="receiver" <?php echo (!empty($receiver_err)) ? 'is-invalid' : ''; ?>>
